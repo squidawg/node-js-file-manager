a194f6619ed3a28bf1fa2f3cc734273fdaf6a1e1
squidawg <78506807+squidawg@users.noreply.github.com>
2023-06-22 15:18:29 +0400

    feat: implement  navigation up function

---
+++ src/navigation/up.js
@@ -0,0 +1,10 @@
+import path from "path";
+import fs from "fs";
+
+export const up = async () => {
+    const currentDir = process.cwd();
+    const parentDir = path.resolve(currentDir, '..');
+    if ( fs.existsSync(parentDir)){
+        process.chdir(parentDir);
+    }
+}
